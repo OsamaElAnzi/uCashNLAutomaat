8001f3129bb9f4b940fcfc88fa3d697be44bfd12
OsamaElAnzi <osamaelanzi0@gmail.com>
2026-03-16 01:01:03 +0100

    Set up new met juiste schermformaat

--- uCashNLAutomaatUI/src/App.jsx
+++ uCashNLAutomaatUI/src/App.jsx
@@ -6,11 +6,21 @@ import "./View/style.css"
 function App() {
   return (
     <div className="page">
-      <div className="row">
-        <OpnemenView />
-        <StortView />
+      <div className="screen-simulator">
+        <div className="screen-content">
+          <div className="row">
+            <div className="button-slot">
+              <OpnemenView />
+            </div>
+            <div className="button-slot">
+              <StortView />
+            </div>
+          </div>
+          <div className="overview-slot">
+            <OverzichtView />
+          </div>
+        </div>
       </div>
-      <OverzichtView />
     </div>
   )
 }
